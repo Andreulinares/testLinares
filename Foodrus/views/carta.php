d2d3474ed6432e1fa7f29b3675fbe2869cd49e83
Andreu <andreu@prueba.com>
2023-11-14 19:41:05 +0100

    carta actualizacion php/css

--- Foodrus/views/carta.php
+++ Foodrus/views/carta.php
@@ -77,13 +77,13 @@ require __DIR__ . '/../model/ProductoDAO.php';
         
         <div class="col-md-4 card-container">
           <div class="card">
-            <img src="<?= $pizza->getImagen(); ?>" class="card-img-top">
+          <img src="../<?= $pizza->getImagen(); ?>" class="card-img-top img-fluid img-product">
             <div class="card-body">
               <h5 class="card-title"><?= $pizza->getNombre_producto(); ?></h5>
               <p class="card-text">
                 <?= $pizza->getPrecio(); ?> €
-                <svg class="carro" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
-                    <path class="carro-color" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                <svg class="carro" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                    <path class="carro-color" d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/>
                 </svg>
               </p>
             </div>
@@ -104,13 +104,13 @@ require __DIR__ . '/../model/ProductoDAO.php';
 
         <div class="col-md-4 card-container">
           <div class="card">
-            <img src="<?= $bebida->getImagen(); ?>" class="card-img-top">
+            <img src="../<?= $bebida->getImagen(); ?>" class="card-img-top img-fluid img-product">
             <div class="card-body">
               <h5 class="card-title"><?= $bebida->getNombre_producto(); ?></h5>
               <p class="card-text">
                 <?= $bebida->getPrecio(); ?> €
-                <svg class="carro" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
-                    <path class="carro-color" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                <svg class="carro" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                    <path class="carro-color" d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/>
                 </svg>
               </p>
             </div>
