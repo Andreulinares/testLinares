e73a4cbb50a01e7bc28a6a06440a972f07684015
Andreu <andreu@prueba.com>
2023-11-15 10:50:54 +0100

    carta php/css

--- Foodrus/views/carta.php
+++ Foodrus/views/carta.php
@@ -79,9 +79,9 @@ require __DIR__ . '/../model/ProductoDAO.php';
           <div class="card">
           <img src="../<?= $pizza->getImagen(); ?>" class="card-img-top img-fluid img-product">
             <div class="card-body">
-              <h5 class="card-title"><?= $pizza->getNombre_producto(); ?></h5>
+              <h5 class="card-title titulo-producto"><?= $pizza->getNombre_producto(); ?></h5>
               <p class="card-text">
-                <?= $pizza->getPrecio(); ?> €
+                <span class="precio"><?= $pizza->getPrecio(); ?> €</span>
                 <svg class="carro" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
                     <path class="carro-color" d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/>
                 </svg>
@@ -106,9 +106,36 @@ require __DIR__ . '/../model/ProductoDAO.php';
           <div class="card">
             <img src="../<?= $bebida->getImagen(); ?>" class="card-img-top img-fluid img-product">
             <div class="card-body">
-              <h5 class="card-title"><?= $bebida->getNombre_producto(); ?></h5>
+              <h5 class="card-title titulo-producto"><?= $bebida->getNombre_producto(); ?></h5>
               <p class="card-text">
-                <?= $bebida->getPrecio(); ?> €
+                <span class="precio"><?= $bebida->getPrecio(); ?> €</span>
+                <svg class="carro" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                    <path class="carro-color" d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/>
+                </svg>
+              </p>
+            </div>
+          </div>
+        </div>
+        <?php
+        endforeach;
+        ?>
+      </div>
+      <h3 class="subtitulo">POSTRES</h3>
+      <div class="row">
+        <?php
+        $postresIds = [23, 24, 25];
+        $postres = ProductoDAO::getProductsByIds('postre', $postresIds);
+
+        foreach ($postres as $postre):
+        ?>
+
+        <div class="col-md-4 card-container">
+          <div class="card">
+            <img src="../<?= $postre->getImagen(); ?>" class="card-img-top img-fluid img-product">
+            <div class="card-body">
+              <h5 class="card-title titulo-producto"><?= $postre->getNombre_producto(); ?></h5>
+              <p class="card-text">
+                <span class="precio"><?= $postre->getPrecio(); ?> €</span>
                 <svg class="carro" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
                     <path class="carro-color" d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/>
                 </svg>
