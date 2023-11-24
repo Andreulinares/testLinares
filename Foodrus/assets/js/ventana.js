86c93f2d06193fe63aa131f75098439720b3f416
Andreu <andreu@prueba.com>
2023-11-24 17:54:15 +0100

    modificaciones carrito y carta

---
+++ Foodrus/assets/js/ventana.js
@@ -0,0 +1,22 @@
+function mostrarVentana() {
+    let ventana = document.getElementById("ventana");
+    let fondoOscuro = document.getElementById("fondoOscuro");
+    ventana.style.display = 'block';
+    fondoOscuro.style.display = 'block';
+
+    document.getElementById('btnFinalizarCompra').addEventListener('click', function() {
+        window.location.href = 'panelCompra.php';
+    });
+
+    document.getElementById('btnContinuarComprando').addEventListener('click', function() {
+        ventana.style.display = 'none'; 
+        fondoOscuro.style.display = 'none';
+    });
+
+    window.onclick = function(event) {
+        if (event.target === ventana) {
+            ventana.style.display = 'none';
+            fondoOscuro.style.display = 'none';
+        }
+    };
+}
