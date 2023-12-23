ce56eb94300515aeb8a7a55fd9fc11eba7822bb5
Andreu <andreu@prueba.com>
2023-12-23 15:29:08 +0100

    editarusuario.php css

--- Foodrus/views/editarUsuario.php
+++ Foodrus/views/editarUsuario.php
@@ -5,29 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Actualizar datos</title>
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link href="../assets/css/editarUsuario.css" rel="stylesheet" type="text/css" media="screen">
 </head>
 <body>
+    <h2>DATOS PERSONALES</h2>
+    <hr class="linea">
+
     <form action="index.php?controller=usuario&action=actualizar" method="post">
         <input type="hidden" name="id" value="<?= $usuario->getCliente_id()?>">
         <div class="form-group">
             <label for="nombre">Nombre</label>
-            <input type="text" id="nombre" name="nombre" value="<?= $usuario->getNombre(); ?>" required>
+            <input type="text" class="form-control label-form" id="nombre" name="nombre" value="<?= $usuario->getNombre(); ?>" required>
         </div>
         <div class="form-group">
             <label for="apell">Apellidos</label>
-            <input type="text" id="apell" name="apell" value="<?= $usuario->getApellidos(); ?>" required>
+            <input type="text" class="form-control label-form" id="apell" name="apell" value="<?= $usuario->getApellidos(); ?>" required>
         </div>
         <div class="form-group">
             <label for="tel">Numero de telefono:</label>
-            <input type="number" id="tel" name="tel" value="<?= $usuario->getTelefono(); ?>" required>
+            <input type="number" class="form-control label-form" id="tel" name="tel" value="<?= $usuario->getTelefono(); ?>" required>
         </div>
         <div class="form-group">
             <label for="mail">Correo electrónico</label>
-            <input type="email" id="mail" name="mail" value="<?= $usuario->getEmail(); ?>" required>
+            <input type="email" class="form-control label-form" id="mail" name="mail" value="<?= $usuario->getEmail(); ?>" required>
         </div>
         <div class="form-group">
             <label for="passwd">Contraseña:</label>
-            <input type="password" id="passwd" name="passwd" value="<?= $usuario->getPassword(); ?>" required>
+            <input type="password" class="form-control label-form" id="passwd" name="passwd" value="<?= $usuario->getPassword(); ?>" required>
         </div>
 
         <button type="submit" name="act-datos">Actualizar</button>
