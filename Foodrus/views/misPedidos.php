f426daae5884fd6de919799174efb882dbc0515f
Andreu <andreu@prueba.com>
2024-01-04 19:06:26 +0100

    arreglo problema con modal detalles pedido

--- Foodrus/views/misPedidos.php
+++ Foodrus/views/misPedidos.php
@@ -150,7 +150,7 @@ require_once __DIR__ . '/../model/ProductoDAO.php';
                     <div class="modal-content">
                         <div class="modal-header">
                             <h5 class="modal-title" id="detallesModalLabel">Detalles del Pedido <?= $pedido->getPedido_id(); ?></h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
@@ -168,7 +168,7 @@ require_once __DIR__ . '/../model/ProductoDAO.php';
                             ?>
                         </div>
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
+                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
                         </div>
                     </div>
                 </div>
