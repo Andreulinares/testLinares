d3416c3d55495ad92b6fb930face4123fffd0240
Andreu <andreu@prueba.com>
2024-01-05 14:59:12 +0100

    seccion menu temporal carta.php y carta.css

--- Foodrus/views/carta.php
+++ Foodrus/views/carta.php
@@ -251,12 +251,65 @@ session_start();
 
             <?php $tarjetasPorFila++; ?>
         <?php endforeach; ?>                  
+    </div>
+    </div>
 
+    <h2 id="especial" class="t-menu">MENU TEMPORAL</h2>
+
+    <div class="container">
+        <div class="row tarjetas">
+            <?php
+            $pizzasIds3 = [29, 30];
+            $pizzas3 = ProductoDAO::getProductsByIds('pizza', $pizzasIds3);                
+
+            $postresIds3 = [31];
+            $postres3 = ProductoDAO::getProductsByIds('postre', $postresIds3);
+
+            $productos3 = array_merge($pizzas3, $postres3);
+
+            $tarjetasPorFila = 0;
+
+            foreach ($productos3 as $index => $producto3):
+                $esExclusivo = ($index % 2 == 1);
+                $specialClass = $esExclusivo ? 'special-card2' : '';
+            ?>
+                <?php if ($tarjetasPorFila % 4 == 0): ?>
+                    </div><div class="row tarjetas">
+                <?php endif; ?>
+
+                <div class="col-md-3 card-container <?= $specialClass ?>">
+                    <div class="card">
+                        <img src="../<?= $producto3->getImagen(); ?>" class="card-img-top img-fluid img-product">
+                        <div class="card-body">
+                            <?php if ($esExclusivo) : ?>
+                                <p class="exclusivo">Exclusivo</p>
+                            <?php endif; ?>
+                            <h5 class="card-title titulo-producto"><?= $producto3->getNombre_producto(); ?></h5>
+                            <p class="card-text">
+                                <span class="precio"><?= $producto3->getPrecio(); ?> €</span>
+                                <form action="../index.php?controller=producto&action=sel" method="post">
+                                    <input type="hidden" name="id" value="<?= $producto3->getProducto_id(); ?>">
+                                    <input type="hidden" name="categoria" value="<?= $producto3->getCategoria(); ?>">
+                                    <button type="submit" name="añadir-carrito" class="carro-btn">
+                                        <svg class="carro" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                                            <path class="carro-color"
+                                                d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/>
+                                        </svg>
+                                    </button>
+                                </form>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                
+                <?php $tarjetasPorFila++; ?>
+            <?php endforeach; ?>
         </div>
         <div class="productos-totales">
             <?= $totalProductos ?> productos encontrados
-        </div>
+        </div>  
     </div>
+
 </section>
 
 <footer>
