6a30988e709ee2053640a95c05612cf219cd1e23
Andreu <andreu@prueba.com>
2024-01-23 19:46:13 +0100

    ajuste css

--- Foodrus/views/misPedidos.php
+++ Foodrus/views/misPedidos.php
@@ -27,77 +27,78 @@ if (isset($_SESSION['user_email'])){
 
     <?php include('header.php'); ?>
     
-    <?php if (isset($esAdmin) && $esAdmin) : ?>
-        <h2 class="mispedidos">Todos los pedidos realizados</h2>
-    <?php else : ?>
-        <h2 class="mispedidos">Mis pedidos</h2>
-    <?php endif; ?>
+    <section class="pedidos">
+        <?php if (isset($esAdmin) && $esAdmin) : ?>
+            <h2 class="mispedidos">Todos los pedidos realizados</h2>
+        <?php else : ?>
+            <h2 class="mispedidos">Mis pedidos</h2>
+        <?php endif; ?>
 
-    <table class="table table-bordered table-striped">
-        <tr>
-            <th scope="col">Num Pedido</th>
-            <th scope="col">Cantidad</th>
-            <th scope="col">Estado</th>
-            <th scope="col">Fecha pedido</th>
-            <th scope="col">Acciones</th>
-            <th scope="col"></th>
-        </tr>
-        <?php foreach ($pedidos as $pedido) : ?>
+        <table class="table table-bordered table-striped">
             <tr>
-                <td><?= $pedido->getPedido_id(); ?></td>
-                <td>€<?= $pedido->getCantidad(); ?></td>
-                <td><?= $pedido->getEstado(); ?></td>
-                <td><?= $pedido->getFecha_pedido(); ?></td>
-                <td>
-                    <?php if ($rol == 'administrador') : ?>
-                        <form action="index.php?controller=usuario&action=finalizarPedido" method="post">
-                            <input type="hidden" name="pedido_id" value="<?= $pedido->getPedido_id(); ?>">
-                            <input type="hidden" name="nuevo_estado" value="entregado">
-                            <button type="submit" class="btn btn-success">Finalizado</button>
-                        </form>
-                    <?php else: ?>
-                        <form action="index.php?controller=usuario&action=eliminarPedido" method="post">
-                            <input type="hidden" name="pedido_id" value="<?= $pedido->getPedido_id(); ?>">
-                            <button type="submit" class="btn btn-danger">Eliminar</button>
-                        </form>
-                    <?php endif; ?>
-                </td>
-                <td>
-                    <button type="button" class="btn btn-secondary ver-detalles" data-bs-toggle="modal" data-bs-target="#detallesModal-<?= $pedido->getPedido_id(); ?>">Ver Detalles</button>
-                </td>
+                <th scope="col">Num Pedido</th>
+                <th scope="col">Cantidad</th>
+                <th scope="col">Estado</th>
+                <th scope="col">Fecha pedido</th>
+                <th scope="col">Acciones</th>
+                <th scope="col"></th>
             </tr>
-<!-- VENTANA DETALLES PEDIDO -->
-            <div class="modal fade" id="detallesModal-<?= $pedido->getPedido_id(); ?>" tabindex="-1" role="dialog" aria-labelledby="detallesModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="detallesModalLabel">Detalles del Pedido <?= $pedido->getPedido_id(); ?></h5>
-                            <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <?php
-                            // Obtener los detalles de los productos para este pedido
-                            $detallesProductos = ProductoDAO::obtenerDetallesProductos($pedido->getPedido_id());
+            <?php foreach ($pedidos as $pedido) : ?>
+                <tr>
+                    <td><?= $pedido->getPedido_id(); ?></td>
+                    <td>€<?= $pedido->getCantidad(); ?></td>
+                    <td><?= $pedido->getEstado(); ?></td>
+                    <td><?= $pedido->getFecha_pedido(); ?></td>
+                    <td>
+                        <?php if ($rol == 'administrador') : ?>
+                            <form action="index.php?controller=usuario&action=finalizarPedido" method="post">
+                                <input type="hidden" name="pedido_id" value="<?= $pedido->getPedido_id(); ?>">
+                                <input type="hidden" name="nuevo_estado" value="entregado">
+                                <button type="submit" class="btn btn-success">Finalizado</button>
+                            </form>
+                        <?php else: ?>
+                            <form action="index.php?controller=usuario&action=eliminarPedido" method="post">
+                                <input type="hidden" name="pedido_id" value="<?= $pedido->getPedido_id(); ?>">
+                                <button type="submit" class="btn btn-danger">Eliminar</button>
+                            </form>
+                        <?php endif; ?>
+                    </td>
+                    <td>
+                        <button type="button" class="btn btn-secondary ver-detalles" data-bs-toggle="modal" data-bs-target="#detallesModal-<?= $pedido->getPedido_id(); ?>">Ver Detalles</button>
+                    </td>
+                </tr>
+    <!-- VENTANA DETALLES PEDIDO -->
+                <div class="modal fade" id="detallesModal-<?= $pedido->getPedido_id(); ?>" tabindex="-1" role="dialog" aria-labelledby="detallesModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="detallesModalLabel">Detalles del Pedido <?= $pedido->getPedido_id(); ?></h5>
+                                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <?php
+                                // Obtener los detalles de los productos para este pedido
+                                $detallesProductos = ProductoDAO::obtenerDetallesProductos($pedido->getPedido_id());
 
-                            // Mostrar los detalles en una lista
-                            echo '<ul>';
-                            foreach ($detallesProductos as $detalle) {
-                                echo '<li>' . $detalle['nombre_producto'] . ' - Cantidad: ' . $detalle['cantidad'] . '</li>';
-                            }
-                            echo '</ul>';
-                            ?>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
+                                // Mostrar los detalles en una lista
+                                echo '<ul>';
+                                foreach ($detallesProductos as $detalle) {
+                                    echo '<li>' . $detalle['nombre_producto'] . ' - Cantidad: ' . $detalle['cantidad'] . '</li>';
+                                }
+                                echo '</ul>';
+                                ?>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        <?php endforeach; ?>
-    </table>
-
+            <?php endforeach; ?>
+        </table>
+    </section>                            
 <footer>
     <div class="container-fluid p-5 text-white bg-custom">
         <div class="row justify-content-center align-items-center">
