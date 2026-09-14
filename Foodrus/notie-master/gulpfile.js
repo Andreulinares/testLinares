177d2d5ed8e8b9375862be7335c96b2f44877edf
Andreu <andreulinaresfp@ibf.cat>
2026-09-14 16:22:51 +0200

    mejoras web

--- Foodrus/notie-master/gulpfile.js
+++ Foodrus/notie-master/gulpfile.js
@@ -1,20 +1,26 @@
 const Webpack = require('webpack')
 const gulp = require('gulp')
-const webpack = require('gulp-webpack')
-const cssnano = require('gulp-cssnano')
 const rename = require('gulp-rename')
-const sass = require('gulp-sass')
-const del = require('del')
-const run = require('run-sequence')
+const sass = require('gulp-sass')(require('sass'))
 
-gulp.task('clean', () => del(['./dist']))
+// Limpiar dist
+gulp.task('clean', async () => {
+  const { deleteAsync } = await import('del')
+  await deleteAsync(['./dist'])
+})
 
+// Configuración de Webpack
 const webpackConfig = minimize => ({
+  mode: minimize ? 'production' : 'development',
+
+  entry: './src/notie.js',
+
   output: {
     filename: minimize ? 'notie.min.js' : 'notie.js',
     library: 'notie',
     libraryTarget: 'umd'
   },
+
   module: {
     rules: [
       {
@@ -24,36 +30,90 @@ const webpackConfig = minimize => ({
       }
     ]
   },
-  plugins: minimize
-    ? [
-      new Webpack.optimize.UglifyJsPlugin({
-        compress: { warnings: false },
-        mangle: true,
-        sourcemap: false
-      })
-    ]
-    : []
+
+  optimization: {
+    minimize
+  }
 })
 
-gulp.task('script', () => {
-  gulp.src('./src/notie.js')
-  .pipe(webpack(webpackConfig(false), Webpack))
-  .pipe(gulp.dest('./dist'))
-  .pipe(webpack(webpackConfig(true), Webpack))
-  .pipe(gulp.dest('./dist'))
+// Ejecutar Webpack
+function runWebpack(config) {
+  return new Promise((resolve, reject) => {
+    Webpack(config, (err, stats) => {
+      if (err) {
+        reject(err)
+        return
+      }
+
+      if (stats.hasErrors()) {
+        reject(new Error(stats.toString({
+          colors: true
+        })))
+        return
+      }
+
+      console.log(stats.toString({
+        colors: true,
+        modules: false
+      }))
+
+      resolve()
+    })
+  })
+}
+
+// JavaScript
+gulp.task('script', async () => {
+  await runWebpack(webpackConfig(false))
+  await runWebpack(webpackConfig(true))
 })
 
+// CSS
 gulp.task('style', () => {
-  gulp.src(['./src/notie.scss'])
-  .pipe(sass().on('error', sass.logError))
-  .pipe(gulp.dest('./dist'))
-  .pipe(cssnano())
-  .pipe(rename('notie.min.css'))
-  .pipe(gulp.dest('./dist'))
+  return Promise.all([
+    new Promise((resolve, reject) => {
+      gulp.src('./src/notie.scss')
+        .pipe(
+          sass().on('error', function (error) {
+            sass.logError.call(this, error)
+            reject(error)
+          })
+        )
+        .pipe(gulp.dest('./dist'))
+        .on('end', resolve)
+    }),
+
+    new Promise((resolve, reject) => {
+      gulp.src('./src/notie.scss')
+        .pipe(
+          sass({
+            style: 'compressed'
+          }).on('error', function (error) {
+            sass.logError.call(this, error)
+            reject(error)
+          })
+        )
+        .pipe(rename('notie.min.css'))
+        .pipe(gulp.dest('./dist'))
+        .on('end', resolve)
+    })
+  ])
 })
 
-gulp.task('default', ['clean'], () => {
-  run('script', 'style')
-  gulp.watch('./src/notie.scss', ['style'])
-  gulp.watch('./src/notie.js', ['script'])
+// Build completo
+gulp.task(
+  'build',
+  gulp.series('clean', gulp.parallel('script', 'style'))
+)
+
+// Watch
+gulp.task('watch', () => {
+  gulp.watch('./src/notie.scss', gulp.series('style'))
+  gulp.watch('./src/notie.js', gulp.series('script'))
 })
+
+// Default
+gulp.task(
+  'default',
+  gulp.series('clean', gulp.parallel('script', 'style'), 'watch')
+)
